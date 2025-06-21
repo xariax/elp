daef5d39f9756f69f27778888bedc51c7e18d728
Xariax <wojcik.r@onet.pl>
2025-06-21 21:32:26 +0200

    usuwanie kilku plików

--- src/pages/UserPanel.js
+++ src/pages/UserPanel.js
@@ -4,6 +4,16 @@ import { useAuth } from "../context/AuthContext";
 const UserPanel =() => {
 
     const { user, logout } = useAuth();
+
+  // BACKEND INTEGRATION: Pobierz dane użytkownika z API
+  // useEffect(() => {
+  //   const fetchUserData = async () => {
+  //     const response = await apiClient.get('/user/data');
+  //     // ...obsłuż dane
+  //   };
+  //   fetchUserData();
+  // }, []);
+
     return(
         <div>
         <h1>zalogowany jako użytkownik
