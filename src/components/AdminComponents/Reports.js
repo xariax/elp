bb6d63341ab73441ece95a7f283914e0606ae5f2
Xariax <wojcik.r@onet.pl>
2025-06-24 16:33:10 +0200

    usunięcie h2 w report

--- src/components/AdminComponents/Reports.js
+++ src/components/AdminComponents/Reports.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Reports = () => {
   return (
     <div className="raport">
-      <h2>Zarządzanie Maszynami</h2>
       <ul>
         <h3>Tutaj będą raporty</h3>
       </ul>
